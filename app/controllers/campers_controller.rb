35d8c9b1bff7239b9814b4ff96a742d08c9454ea
Vincent <makokhavomondi@gmail.com>
2022-11-28 12:27:45 +0300

    Completed Assignment

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -3,25 +3,25 @@ class CampersController < ApplicationController
     rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
 
     def index
-        render json: Camper.all
+        render json: Camper.all, status: :ok
     end
 
     def show
         camper = Camper.find(params[:id])
-        render json: camper, serializer: CamperActivitySerializer
+        render json: camper, serializer: CamperActivitySerializer, status: :ok
     end
 
     def create
-        render json: Camper.create!(camper_params)
+        render json: Camper.create!(camper_params), status: :created
     end
 
     private
     def record_not_found
-        render json: {error: "Camper not found"}
+        render json: {error: "Camper not found"}, status: 404
     end
 
     def record_invalid(invalid)
-        render json: {errors: invalid.record.errors.full_messages}
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
     end
 
     def camper_params
--- app/models/camper.rb
+++ app/models/camper.rb
@@ -3,5 +3,5 @@ class Camper < ApplicationRecord
     has_many :activities, through: :signups
     
     validates :name, presence: true
-    validates :age, presence: true, numericality: {greater_than: 9, less_than: 18}
+    validates :age, presence: true, numericality: {greater_than_or_equal_to: 8, less_than_or_equal_to: 18}
 end
